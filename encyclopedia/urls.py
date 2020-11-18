8acc7caca496c67ee5aadfc44ddce2bfd28829d5
Andre Vicente <isquicha@gmail.com>
2020-11-18 01:05:51 -0300

    Add wiki page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("/wiki/<str:entry>", views.wiki, name="wiki"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(
+        request, "encyclopedia/index.html", {"entries": util.list_entries()}
+    )
 
+
+def wiki(request, entry):
+    content = util.get_entry(entry)
+    return render(
+        request,
+        "encyclopedia/wiki.html",
+        {"title": entry, "content": Markdown().convert(content)},
+    )
